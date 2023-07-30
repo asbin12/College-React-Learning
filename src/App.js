9e4a155b005c6fd190c0b28c113b9ef661d5ea5d
Asbin <bhattaraiasbin01@gmail.com>
2023-07-30 18:51:05 +0545

    Changes minor

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 // import './App.css';
 import AppApp from "./AppApp";
 import BmiCal from "./BMI_calc/BmiCal";
-import Nav from "./NavBar/Navbar";
 
 function App() {
   return (
     <div>
-      {/* <AppApp /> */}
-      <Nav/>
+      <AppApp />
 
     </div>
   );
